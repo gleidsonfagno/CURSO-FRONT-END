49b1f5016113049c76bb79bd06d887c601aa5408
gleidson fagno <gleidsnfagner838@gmail.com>
2022-09-25 23:54:43 -0300

    atividade do modulo 13 de javascript

--- javascript/js/script-md-2.js
+++ javascript/js/script-md-2.js
@@ -1,6 +1,6 @@
 function calcularMedia ( notas){
 
-    var soma = 0;
+    let soma = 0;
     for ( c = 0; c < notas.length; c++){
         soma += notas[c];
     }
@@ -10,15 +10,28 @@ function calcularMedia ( notas){
     return media;
 }
 
+let media; //escopo global
+
 function aprovacao( notas){
-    let media = calcularMedia( notas );
-    let condicao = media >= 7 ? "aprovado" : "reprovado";
+    let media = calcularMedia( notas ); //escopo da funcao
+    let condicao = media >= 8 ? "aprovado" : "reprovado";
 
     return "Media" + media + "- resultado" + condicao;
+}
+// funcao recursiva
+
+function contagemRegressiva(numero){
+    console.log(numero);
+
+    let aproximoNumero = numero - 1;
+
+    if (proximoNumero > 0)
+    contagemRegressiva(proximoNumero);     
 }
 
 document.addEventListener("submit", function(evento){
     evento.preventDefault();
+    evento.stopPropagation();
 
     let formulario = document.getElementById("formulario-01");
 
@@ -26,9 +39,20 @@ document.addEventListener("submit", function(evento){
 
     let objeto = {};
 
+    let notas = [];
+
     for(let key of dados.keys()){
         objeto[key] = dados.get(key);
+
+        notas.push(dados.get(key));
     }
 
+    console.log()
+
     console.log(objeto);
+
+    aprovacao( notas);
+
+    document.getElementById("resultado").innerHTML = aprovacao(notas);
+
 });
